3c2dd1af045360cac92bdf0c69e1458168993018
100168747 <whatsitsname123@gmail.com>
2019-10-01 15:50:36 -0500

    made floating point

--- venv/Learning.py
+++ venv/Learning.py
@@ -3,7 +3,10 @@ print("A FEW THINGS")
 # tells me true or false
 print(5 < 3)
 # whole number
-print(int(9 / 8))
+print(float(9 / 8))
+# floating point whole number
+print(float(9 / 9))
+
 print(6 + 9 + 4 + 20 / 6 - 9)
 # intiger
 print(9 / 8)
