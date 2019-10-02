d3eb7640e55c1867f8e191e982773b62e1a813ec
100168747 <whatsitsname123@gmail.com>
2019-10-02 15:48:24 -0500

    made a thing to tell a story

--- venv/Learning.py
+++ venv/Learning.py
@@ -10,3 +10,22 @@ print(float(9 / 9))
 print(6 + 9 + 4 + 20 / 6 - 9)
 # intiger
 print(9 / 8)
+
+
+
+# variable
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passengers = 90
+cars_driven = drivers
+cars_not_driven = cars - drivers
+carpool_capacity = cars_driven * space_in_car
+average_passengers_per_car = passengers/cars_driven
+
+print("There are", cars,"cars available.")
+print("There are only", drivers,"drivers available.")
+print("There will be", cars_not_driven,"empty cars today.")
+print("We can trasnport", carpool_capacity,"people today.")
+print("We have", passengers,"passengers to carpool today")
+print("We need to put about", average_passengers_per_car,"people in each car")
